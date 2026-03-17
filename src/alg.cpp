d75099bd335b8e5d42c123433df33bc8a3768394
vladsundukov1-bit <vladsundukov1@gmail.com>
2026-03-17 18:32:50 +0300

    Сюндюков Владислав 25-ист-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,15 +3,43 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+  while (num > 1) {
+        if (num % 2 == 0) {
+            num /= 2;
+        } else {
+            num = 3 * num + 1; 
+        }
+        if (num > maxvalue) {
+            maxvalue = num;
+        }
+    }
+  return maxvalue;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+   unsigned int lenn = 1;
+   while (num > 1) {
+        if (num % 2 == 0) {
+            num /= 2;
+        } else {
+            num = 3 * num + 1;
+        }
+        lenn++;
+    }
+   return lenn;
 }
 
 unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t lbound,
                         uint64_t rbound) {
-  return 1;
+  unsigned int bestnum = lbound;
+  *maxlen = 0;
+    for (uint64_t num = lbound; num <= rbound; num++) {
+        unsigned int curlen = collatzLen(num);
+        if (curlen > *maxlen) {
+            *maxlen = curlen;  
+            bestnum = num;
+        }
+    }  
+    return bestnum;
 }
